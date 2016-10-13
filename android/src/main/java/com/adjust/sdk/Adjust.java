7da4d893a35ec159638c85dfaae0e884748d806d
Obaied <abdullah.obaied@gmail.com>
2016-10-13 18:39:12 +0200

    Modify the ReactMethod parameters and returns to accomodate for callbacks

--- android/src/main/java/com/adjust/sdk/Adjust.java
+++ android/src/main/java/com/adjust/sdk/Adjust.java
@@ -5,10 +5,12 @@ import android.view.Gravity;
 import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.*;
 import android.util.Log;
+import android.net.Uri;
 
 import com.adjust.sdk.*;
 
 public class Adjust extends ReactContextBaseJavaModule implements LifecycleEventListener {
+    public static final String TAG = "AdjustNativeModule";
     public Adjust(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -18,11 +20,6 @@ public class Adjust extends ReactContextBaseJavaModule implements LifecycleEvent
         return "Adjust";
     }
 
-    @ReactMethod
-    public void show() {
-        Log.d("AdjustNativeModule", ">>> show() invoked from Android");
-    }
-
     @Override
     public void initialize() {
         getReactApplicationContext().addLifecycleEventListener(this);
@@ -30,16 +27,86 @@ public class Adjust extends ReactContextBaseJavaModule implements LifecycleEvent
 
     @Override
     public void onHostPause() {
-        Log.d("AdjustNativeModule", ">>> onHostPause()");
+        Log.d(TAG, ">>> onHostPause()");
     }
 
     @Override
     public void onHostResume() {
-        Log.d("AdjustNativeModule", ">>> onHostResume()");
+        Log.d(TAG, ">>> onHostResume()");
     }
 
     @Override
     public void onHostDestroy() {
-        Log.d("AdjustNativeModule", ">>> onHostDestroy()");
+        Log.d(TAG, ">>> onHostDestroy()");
+    }
+
+    @ReactMethod
+    public void trackEvent(ReadableMap event) {
+        Log.d(TAG, ">>> trackEvent()");
+    }
+
+    @ReactMethod
+    public void setEnabled(Boolean enabled) {
+        Log.d(TAG, ">>> setEnabled()");
+    }
+
+    @ReactMethod
+    public void isEnabled(Callback callback) {
+        callback.invoke(false);
+    }
+
+    @ReactMethod
+    public void appWillOpenUrl(String uri) {
+        Log.d(TAG, ">>> appWillOpenUrl()");
+    }
+
+    @ReactMethod
+    public void setReferrer(String referrer) {
+        Log.d(TAG, ">>> setReferrer()");
+    }
+
+    @ReactMethod
+    public void setOfflineMode(Boolean enabled) {
+        Log.d(TAG, ">>> setOfflineMode()");
+    }
+
+    @ReactMethod
+    public void sendFirstPackages() {
+        Log.d(TAG, ">>> sendFirstPackages()");
+    }
+
+    @ReactMethod
+    public void addSessionCallbackParameter(String key, String value) {
+        Log.d(TAG, ">>> addSessionCallbackParameter()");
+    }
+
+    @ReactMethod
+    public void addSessionPartnerParameter(String key, String value) {
+        Log.d(TAG, ">>> addSessionPartnerParameter()");
+    }
+
+    @ReactMethod
+    public void removeSessionCallbackParameter(String key) {
+        Log.d(TAG, ">>> removeSessionCallbackParameter()");
+    }
+
+    @ReactMethod
+    public void removeSessionPartnerParameter(String key) {
+        Log.d(TAG, ">>> removeSessionPartnerParameter()");
+    }
+
+    @ReactMethod
+    public void resetSessionCallbackParameters() {
+        Log.d(TAG, ">>> resetSessionCallbackParameters()");
+    }
+
+    @ReactMethod
+    public void resetSessionPartnerParameters() {
+        Log.d(TAG, ">>> resetSessionPartnerParameters()");
+    }
+
+    @ReactMethod
+    public void setPushToken(String token) {
+        Log.d(TAG, ">>> setPushToken()");
     }
 }

5e88db6b2469f8dac03025bdeeb96112cc11ce83
Obaied <abdullah.obaied@gmail.com>
2016-10-28 10:58:04 +0200

    Licensing and versioning

--- ios/adjustSdk.h
+++ ios/adjustSdk.h
@@ -1,3 +1,10 @@
+//
+//  adjustSdk.h
+//  Adjust
+//
+//  Created by Abdullah Obaied on 2016-10-25.
+//  Copyright (c) 2012-2014 adjust GmbH. All rights reserved.
+//
 #import "RCTBridgeModule.h"
 #import "Adjust.h"
 
--- ios/adjustSdk.m
+++ ios/adjustSdk.m
@@ -1,3 +1,10 @@
+//
+//  adjustSdk.m
+//  Adjust
+//
+//  Created by Abdullah Obaied on 2016-10-25.
+//  Copyright (c) 2012-2014 adjust GmbH. All rights reserved.
+//
 #import "RCTBridge.h"
 #import "RCTEventDispatcher.h"
 #import "adjustSdk.h"

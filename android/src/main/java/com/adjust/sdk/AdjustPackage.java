cd3599b31c43bcee79003e057d7b2d27e55cfeb1
Obaied <abdullah.obaied@gmail.com>
2017-08-02 15:29:11 +0200

    Remove createJsModules() method since it was removed in RN 0.47

--- android/src/main/java/com/adjust/sdk/AdjustPackage.java
+++ android/src/main/java/com/adjust/sdk/AdjustPackage.java
@@ -28,11 +28,6 @@ public class AdjustPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }

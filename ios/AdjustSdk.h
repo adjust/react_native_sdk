e43e32936a55ace8b5f120375621be1c6639284c
Obaied <abdullah.obaied@gmail.com>
2017-12-28 14:23:06 +0100

    Reinclude statement for importing RCTBridgeModule.h
    It might be necessary for supporting pre-0.47.0 RN apps

--- ios/AdjustSdk.h
+++ ios/AdjustSdk.h
@@ -7,7 +7,12 @@
 //
 
 #import "Adjust.h"
+
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
+#endif
 
 @interface AdjustSdk : NSObject <RCTBridgeModule>
 

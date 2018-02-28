222d37c5a2450c289e54c3b3169e1492b4f7da0c
Obaied <abdullah.obaied@gmail.com>
2018-02-28 11:35:11 +0100

    Add command_executor.js and update test_app accordingly
    - test_options.js is used in command_executor.js to hold the value of
    AdjustTestOptions

--- test_app/App.js
+++ test_app/App.js
@@ -6,53 +6,86 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    DeviceEventEmitter,
 } from 'react-native';
+import { CommandExecutor } from './command_executor.js';
+import { Adjust, AdjustEvent, AdjustConfig } from 'react-native-adjust';
+
+const {
+    NativeModules,
+} = require('react-native');
+
+const AdjustTesting = NativeModules.AdjustTesting;
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' +
+        'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    componentWillMount() {
+        var baseUrl = "";
+        if (Platform.OS === "android") {
+            baseUrl = "https://10.0.2.2:8443";
+        } else if (Platform.OS === "ios") {
+            baseUrl = "http://127.0.0.1:8080";
+        }
+
+        //AdjustTesting.addTest("current/attributionCallback/Test_AttributionCallback_ask_in_once");
+        AdjustTesting.addTestDirectory("current/offlineMode/");
+        AdjustTesting.startTestSession(baseUrl);
+
+        const commandExecutor = new CommandExecutor(baseUrl);
+        DeviceEventEmitter.addListener('command', (json) => {
+            const commandDict  = JSON.parse(json);
+            const className    = commandDict['className'];
+            const functionName = commandDict['functionName'];
+            const params       = commandDict['params'];
+            const order        = commandDict['order'];
+
+            commandExecutor.scheduleCommand(className, functionName, params, order);
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                {instructions}
+                </Text>
+                </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
---
+++ test_app/test_options.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export function AdjustTestOptions() {
+    this.hasContext                       = false;
+    this.baseUrl                          = null;
+    this.basePath                         = null;
+    this.useTestConnectionOptions         = null;
+    this.timerIntervalInMilliseconds      = null;
+    this.timerStartInMilliseconds         = null;
+    this.sessionIntervalInMilliseconds    = null;
+    this.subsessionIntervalInMilliseconds = null;
+    this.teardown                         = null;
+};

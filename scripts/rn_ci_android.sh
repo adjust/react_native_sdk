92879be0e7525d98bfd5011755dc13d86e4d8d87
Obaied <abdullah.obaied@gmail.com>
2018-02-28 11:30:56 +0100

    Update test ci script

--- scripts/rn_ci_android.sh
+++ scripts/rn_ci_android.sh
@@ -17,43 +17,43 @@ RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-# Kill any previously running packager instance
-killall -9 node
+# Kill any previously running packager instances
+#killall -9 node || true
 
 echo -e "${GREEN}>>> Removing Android JARs ${NC}"
 cd ${ROOT_DIR}
 rm -rfv android/libs/*
-rm -rfv test_plugin/android/libs/*
+rm -rfv test_plugin/android/libs/adjust-testing.jar
 
 echo -e "${GREEN}>>> Removing app from device ${NC}"
-adb uninstall com.adjust.testapp
+adb uninstall com.adjust.testapp || true
 
-# Building the Android JAR file
 echo -e "${GREEN}>>> Building the Android JARs ${NC}"
 cd ${ROOT_DIR}
 ext/android/build.sh release
 ext/android/build_test_lib.sh 
 
-# Remove node_modules from the example project
 echo -e "${GREEN}>>> Removing current module ${NC}"
 cd ${ROOT_DIR}/${PROJECT_DIR}
-react-native uninstall ${SDK_PLUGIN_NAME}
-react-native uninstall ${TEST_PLUGIN_NAME}
+react-native unlink ${SDK_PLUGIN_NAME}
+react-native unlink ${TEST_PLUGIN_NAME}
+react-native uninstall ${SDK_PLUGIN_NAME} || true
+react-native uninstall ${TEST_PLUGIN_NAME} || true
 rm -rfv node_modules/${SDK_PLUGIN_NAME}
 rm -rfv node_modules/${TEST_PLUGIN_NAME}
 
 echo -e "${GREEN}>>> Create new directory in node_modules ${NC}"
 cd ${ROOT_DIR}/${PROJECT_DIR}
-mkdir node_modules/${SDK_PLUGIN_NAME}
-mkdir node_modules/${TEST_PLUGIN_NAME}
+mkdir -p node_modules/${SDK_PLUGIN_NAME}
+mkdir -p node_modules/${TEST_PLUGIN_NAME}
 
 echo -e "${GREEN}>>> Copy modules to ${PROJECT_DIR}/node_modules/${SDK_PLUGIN_NAME} ${NC}"
 cd ${ROOT_DIR}
-rsync -a . ${PROJECT_DIR}/node_modules/${SDK_PLUGIN_NAME} --exclude=example --exclude=ext --exclude=scripts
+rsync -a . ${PROJECT_DIR}/node_modules/${SDK_PLUGIN_NAME} --exclude=example --exclude=ext --exclude=scripts --exclude=test_plugin --exclude=test_app --exclude=.git
 
 echo -e "${GREEN}>>> Copy modules to ${PROJECT_DIR}/node_modules/${TEST_PLUGIN_NAME} ${NC}"
-cd ${ROOT_DIR} 
-rsync -a ${TEST_PLUGIN_DIR} ${PROJECT_DIR}/node_modules/${TEST_PLUGIN_NAME}
+cd ${ROOT_DIR}/${TEST_PLUGIN_DIR}
+rsync -a . ${ROOT_DIR}/${PROJECT_DIR}/node_modules/${TEST_PLUGIN_NAME}
 
 echo -e "${GREEN}>>> Establish linkages ${NC}"
 cd ${ROOT_DIR}/${PROJECT_DIR}

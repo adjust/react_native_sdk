2391e21e16dd127683ba0203ed4276b653db185d
uerceg <ugi@adjust.com>
2018-12-11 11:42:39 +0100

    Update build script to use new Android paths

--- scripts/build_sdk_android.py
+++ scripts/build_sdk_android.py
@@ -14,20 +14,24 @@ def build(root_dir, android_submodule_dir, with_test_lib, is_release = True):
     # ------------------------------------------------------------------
     # Running make*Jar Gradle task ...
     if is_release:
-        debug_green('Running makeReleaseJar Gradle task ...')
-        jar_in_dir = '{0}/adjust/build/intermediates/intermediate-jars/release'.format(build_dir)
-        subprocess.call(['./gradlew', 'makeReleaseJar'])
+        debug_green('Running adjustSdkNonNativeJarRelease Gradle task ...')
+        jar_in_dir = '{0}/sdk-core/build/libs'.format(build_dir)
+        subprocess.call(['./gradlew', 'adjustSdkNonNativeJarRelease'])
     else:
-        debug_green('Running makeDebugJar Gradle task ...')
-        jar_in_dir = '{0}/adjust/build/intermediates/intermediate-jars/debug'.format(build_dir)
-        subprocess.call(['./gradlew', 'makeDebugJar'])
+        debug_green('Running adjustSdkNonNativeJarDebug Gradle task ...')
+        jar_in_dir = '{0}/sdk-core/build/libs'.format(build_dir)
+        subprocess.call(['./gradlew', 'adjustSdkNonNativeJarDebug'])
 
     # ------------------------------------------------------------------
     # Moving Android SDK JAR from jarIn to jarOut dir
     debug_green('Moving Android SDK JAR from {0} to {1} dir ...'.format(jar_in_dir, jar_out_dir))
     clear_dir(jar_out_dir)
-    copy_files('classes.jar', jar_in_dir, jar_out_dir)
-    rename_file('classes.jar', 'adjust-android.jar', jar_out_dir)
+    if is_release:
+        copy_files('adjust-sdk-release.jar', jar_in_dir, jar_out_dir)
+        rename_file('adjust-sdk-release.jar', 'adjust-android.jar', jar_out_dir)
+    else:
+        copy_files('adjust-sdk-debug.jar', jar_in_dir, jar_out_dir)
+        rename_file('adjust-sdk-debug.jar', 'adjust-android.jar', jar_out_dir)
 
     if with_test_lib:
         # ------------------------------------------------------------------
@@ -35,20 +39,20 @@ def build(root_dir, android_submodule_dir, with_test_lib, is_release = True):
         set_log_tag('ANROID-TEST-LIB-BUILD')
         waiting_animation(duration=4.0, step=0.025)
         debug_green('Building Test Library started ...')
-        test_jar_in_dir  = '{0}/testlibrary/build/outputs'.format(build_dir)
+        test_jar_in_dir  = '{0}/test-library/build/libs'.format(build_dir)
         test_jar_out_dir = '{0}/test/lib/android/libs'.format(root_dir)
 
         if not os.path.exists(test_jar_out_dir):
             os.makedirs(test_jar_out_dir)
 
         # ------------------------------------------------------------------
-        # Running Gradle tasks: clean testlibrary:makeJar ...
-        debug_green('Running Gradle tasks: clean testlibrary:makeJar ...')
+        # Running Gradle tasks: clean test-library:adjustMakeJarDebug.
+        debug_green('Running Gradle tasks: clean test-library:adjustMakeJarDebug ...')
         os.chdir(build_dir)
-        subprocess.call(['./gradlew', 'clean', ':testlibrary:makeJar'])
+        subprocess.call(['./gradlew', 'clean', ':test-library:adjustMakeJarDebug'])
 
         # ------------------------------------------------------------------
         # Moving the generated Android SDK JAR from jar in to jar out dir ...
         debug_green('Moving the generated Android SDK JAR from {0} to {1} dir ...'.format(test_jar_in_dir, test_jar_out_dir))
-        copy_files('*.jar', test_jar_in_dir, test_jar_out_dir)
-        rename_file('*.jar', 'adjust-testing.jar', test_jar_out_dir)
+        copy_files('test-library-debug.jar', test_jar_in_dir, test_jar_out_dir)
+        rename_file('test-library-debug.jar', 'adjust-testing.jar', test_jar_out_dir)

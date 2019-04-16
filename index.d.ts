351fa00f344c7a3ae4adba3045ae19cc39e0ccf0
serj <srdjan@adjust.com>
2019-04-16 19:07:28 +0200

    event, config and API full support in typescript

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,7 @@ declare module "react-native-adjust" {
         Assert,
         Suppress
     }
-  
+
     interface AdjustAttribution {
         trackerToken: string;
         trackerName: string;
@@ -21,7 +21,7 @@ declare module "react-native-adjust" {
         clickLabel: string;
         adid: string;
     }
-  
+
     interface AdjustEventTrackingSuccess {
         message: string;
         timestamp: string;
@@ -47,7 +47,7 @@ declare module "react-native-adjust" {
         adid: string;
         jsonResponse: string;
     }
-  
+
     interface AdjustSessionTrackingFailure {
         message: string;
         timestamp: string;
@@ -59,68 +59,118 @@ declare module "react-native-adjust" {
     interface AdjustUri {
         uri: string;
     }
-  
+
     export class AdjustConfig {
-      constructor(appToken: string, environment: Environment) {}
-  
-      public setLogLevel(level: LogLevel): void {}
+        constructor(appToken: string, environment: Environment) { }
 
-      public setAttributionCallbackListener(callback: (attribution: AdjustAttribution) => void): void {}
+        public setLogLevel(level: LogLevel): void { }
 
-      public setEventTrackingSucceededCallbackListener(callback: (eventSuccess: AdjustEventTrackingSuccess) => void): void {}
+        public setEventBufferingEnabled(isEnabled: boolean): void { }
 
-      public setEventTrackingFailedCallbackListener(callback: (eventFailed: AdjustEventTrackingFailure) => void): void {}
+        public setProcessName(processName: string): void { }
 
-      public setSessionTrackingSucceededCallbackListener(callback: (sessionSuccess: AdjustSessionTrackingSuccess) => void): void {}
+        public setDefaultTracker(defaultTracked: string): void { }
 
-      public setSessionTrackingFailedCallbackListener(callback: (sessionFailed: AdjustSessionTrackingFailure) => void): void {}
+        public setUserAgent(userAgent: string): void { }
 
-      public setDeferredDeeplinkCallbackListener(callback: (uri: AdjustUri) => void): void {}
-  
-      static get LogLevelVerbose(): LogLevel {
-        return LogLevel.Verbose;
-      }
-  
-      static get LogLevelDebug(): LogLevel {
-        return LogLevel.Debug;
-      }
-  
-      static get LogLevelInfo(): LogLevel {
-        return LogLevel.Info;
-      }
-  
-      static get LogLevelWarn(): LogLevel {
-        return LogLevel.Warn;
-      }
-  
-      static get LogLevelError(): LogLevel {
-        return LogLevel.Error;
-      }
-  
-      static get LogLevelAssert(): LogLevel {
-        return LogLevel.Assert;
-      }
-  
-      static get LogLevelSuppress(): LogLevel {
-        return LogLevel.Suppress;
-      }
-  
-      static get EnvironmentSandbox(): Environment {
-        return "sandbox";
-      }
+        public setAppSecret(secretId: number, info1: number, info2: number, info3: number, info4: number): void { }
 
-      static get EnvironmentProduction(): Environment {
-        return "production";
-      }
+        public setDelayStart(delayStart: number): void { }
+
+        public setSendInBackground(sendInBackground: boolean): void { }
+
+        public setDeviceKnown(isDeviceKnown: boolean): void { }
+
+        public setSdkPrefix(sdkPrefix: string): void { }
+
+        public setShouldLaunchDeeplink(shouldLaunchDeeplink: boolean): void { }
+
+        public setAttributionCallbackListener(callback: (attribution: AdjustAttribution) => void): void { }
+
+        public setEventTrackingSucceededCallbackListener(callback: (eventSuccess: AdjustEventTrackingSuccess) => void): void { }
+
+        public setEventTrackingFailedCallbackListener(callback: (eventFailed: AdjustEventTrackingFailure) => void): void { }
+
+        public setSessionTrackingSucceededCallbackListener(callback: (sessionSuccess: AdjustSessionTrackingSuccess) => void): void { }
+
+        public setSessionTrackingFailedCallbackListener(callback: (sessionFailed: AdjustSessionTrackingFailure) => void): void { }
+
+        public setDeferredDeeplinkCallbackListener(callback: (uri: AdjustUri) => void): void { }
+
+        static get LogLevelVerbose(): LogLevel {
+            return LogLevel.Verbose;
+        }
+
+        static get LogLevelDebug(): LogLevel {
+            return LogLevel.Debug;
+        }
+
+        static get LogLevelInfo(): LogLevel {
+            return LogLevel.Info;
+        }
+
+        static get LogLevelWarn(): LogLevel {
+            return LogLevel.Warn;
+        }
+
+        static get LogLevelError(): LogLevel {
+            return LogLevel.Error;
+        }
+
+        static get LogLevelAssert(): LogLevel {
+            return LogLevel.Assert;
+        }
+
+        static get LogLevelSuppress(): LogLevel {
+            return LogLevel.Suppress;
+        }
+
+        static get EnvironmentSandbox(): Environment {
+            return "sandbox";
+        }
+
+        static get EnvironmentProduction(): Environment {
+            return "production";
+        }
     }
 
     export class AdjustEvent {
-        constructor(eventToken: string) {}
+        constructor(eventToken: string) { }
+
+        public setRevenue(revenue: number, currency: string): void { }
+
+        public addCallbackParameter(key: string, value: string): void { }
+
+        public addPartnerParameter(key: string, value: string): void { }
+
+        public setTransactionId(transactionId: string): void { }
+
+        public setCallbackId(callbackId: string): void { }
     }
-  
+
     export const Adjust = {
-      componentWillUnmount: (): void => {},
-      create: (adjustConfig: AdjustConfig): void => {},
-      trackEvent: (adjustEvent: AdjustEvent): void => {},
+        componentWillUnmount: (): void => { },
+        create: (adjustConfig: AdjustConfig): void => { },
+        trackEvent: (adjustEvent: AdjustEvent): void => { },
+        setEnabled: (enabled: boolean): void => { },
+        isEnabled: (callback: (enabled: boolean) => void): void => { },
+        setOfflineMode: (enabled: boolean): void => { },
+        setPushToken: (token: string): void => { },
+        appWillOpenUrl: (url: string): void => { },
+        sendFirstPackages: (): void => { },
+        addSessionCallbackParameter: (key: string, value: string): void => { },
+        addSessionPartnerParameter: (key: string, value: string): void => { },
+        removeSessionCallbackParameter: (key: string): void => { },
+        removeSessionPartnerParameter: (key: string): void => { },
+        resetSessionCallbackParameters: (): void => { },
+        resetSessionPartnerParameters: (): void => { },
+        gdprForgetMe: (): void => { },
+        getIdfa: (callback: (idfa: string) => void): void => { },
+        getGoogleAdId: (callback: (adid: string) => void): void => { },
+        getAdid: (callback: (adid: string) => void): void => { },
+        getAttribution: (callback: (attribution: AdjustAttribution) => void): void => { },
+        getAmazonAdId: (callback: (adid: string) => void): void => { },
+        getSdkVersion: (callback: (sdkVersion: string) => void): void => { },
+        setReferrer: (referrer: string): void => { },
     };
 }

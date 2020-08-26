f9c0511c1ad6afd386bfec4dc2d0fcb1db02eafc
ugi <ugi@adjust.com>
2020-08-26 14:55:42 +0200

    new version 4.23.0

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ Adjust.getAmazonAdId = function(callback) {
 };
 
 Adjust.getSdkVersion = function(callback) {
-    module_adjust.getSdkVersion("react-native4.22.0", callback);
+    module_adjust.getSdkVersion("react-native4.23.0", callback);
 };
 
 Adjust.setReferrer = function(referrer) {
@@ -208,7 +208,7 @@ Adjust.onPause = function(testParam) {
 // AdjustConfig
 
 var AdjustConfig = function(appToken, environment) {
-    this.sdkPrefix = "react-native4.22.0";
+    this.sdkPrefix = "react-native4.23.0";
     this.appToken = appToken;
     this.environment = environment;
     this.logLevel = null;

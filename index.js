2233d9772cc2613a06d35d4b6268c8c61149eff8
uerceg <ugi@adjust.com>
2022-10-10 23:42:22 +0200

    feat: udpate version to 4.32.0

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ Adjust.getAmazonAdId = function(callback) {
 };
 
 Adjust.getSdkVersion = function(callback) {
-    module_adjust.getSdkVersion("react-native4.31.0", callback);
+    module_adjust.getSdkVersion("react-native4.32.0", callback);
 };
 
 Adjust.setReferrer = function(referrer) {
@@ -238,7 +238,7 @@ Adjust.onPause = function(testParam) {
 // AdjustConfig
 
 var AdjustConfig = function(appToken, environment) {
-    this.sdkPrefix = "react-native4.31.0";
+    this.sdkPrefix = "react-native4.32.0";
     this.appToken = appToken;
     this.environment = environment;
     this.logLevel = null;

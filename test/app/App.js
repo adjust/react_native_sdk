8412604f207d907cb55d8d0a6c5b72fe579bec9f
uerceg <ugi@adjust.com>
2023-10-05 15:52:58 +0200

    chore: nudge test apps

--- test/app/App.js
+++ test/app/App.js
@@ -50,7 +50,7 @@ const App: () => React$Node = () => {
   var baseUrl = "";
   var gdprUrl = "";
   var subscriptionUrl = "";
-  var ipAddress = "192.168.86.111";
+  var ipAddress = "192.168.86.40";
   if (Platform.OS === "android") {
     baseUrl = "https://" + ipAddress + ":8443";
     gdprUrl = "https://" + ipAddress + ":8443";

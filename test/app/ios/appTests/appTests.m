b8b978ece70b9111075e846ba4c81607ac89c7ab
uerceg <ugi@adjust.com>
2024-08-28 12:58:12 +0200

    chore: recreate test app

--- test/app/ios/appTests/appTests.m
+++ test/app/ios/appTests/appTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AdjustTestTests : XCTestCase
+@interface appTests : XCTestCase
 
 @end
 
-@implementation AdjustTestTests
+@implementation appTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

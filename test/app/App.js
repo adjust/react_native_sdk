203dbb7010ca556a4676094b3f1ce60cfd3be83f
uerceg <ugi@adjust.com>
2024-09-03 13:13:45 +0200

    fix: switch to android sdk v5 test library handling

--- test/app/App.js
+++ test/app/App.js
@@ -48,7 +48,7 @@ const App: () => React$Node = () => {
   }
 
   var urlOverwrite = "";
-  var ipAddress = "192.168.8.60";
+  var ipAddress = "192.168.8.202";
   if (Platform.OS === "android") {
     urlOverwrite = "https://" + ipAddress + ":8443";
   } else if (Platform.OS === "ios") {
--- test/lib/android/src/main/java/com/adjust/test/AdjustSdkTest.java
+++ test/lib/android/src/main/java/com/adjust/test/AdjustSdkTest.java
@@ -33,7 +33,7 @@ public class AdjustSdkTest extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void startTestSession(String baseUrl, String controlUrl, String sdkVersion) {
-        testLibrary = new TestLibrary(baseUrl, controlUrl, new CommandListener(getReactApplicationContext()));
+        testLibrary = new TestLibrary(baseUrl, controlUrl, getReactApplicationContext(), new CommandListener(getReactApplicationContext()));
         for (int i = 0; i < selectedTests.size(); i++) {
             testLibrary.addTest(selectedTests.get(i));
         }

da554d6d63b8a02eff8eda128bc10957ebee11c6
uerceg <ugi@adjust.com>
2026-04-28 15:07:18 +0200

    align test app with react native 0.83.5

--- test/app/App.js
+++ test/app/App.js
@@ -8,35 +8,21 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
   NativeEventEmitter,
+  NativeModules,
+  Platform,
 } from 'react-native';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import {
   CommandExecutor
 } from './command_executor.js';
 import {
-  Adjust,
-  AdjustEvent,
-  AdjustConfig
+  Adjust
 } from 'react-native-adjust';
 
 const App: () => React$Node = () => {
-  const {
-    NativeModules,
-  } = require('react-native');
-
   const AdjustSdkTest = NativeModules.AdjustSdkTest;
 
   let AdjustSdkTestEmitter = null;
@@ -83,42 +69,6 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
   container: {
     flex: 1,
     justifyContent: 'center',
